b86e5ba295b5e98ee5b096f99e3643bada3fc756
PresentKim <debe3721@gmail.com>
2024-10-31 18:56:20 +0900

    feat(server): 스프링 서버 프로젝트 생성

---
+++ server/src/main/java/dev/repostory/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package dev.repostory.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
